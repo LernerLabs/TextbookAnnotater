d3f555caf3815815f22e095bef2c2cc5eaf200a1
AF3 <af@af3.tech>
2021-05-28 10:26:34 -0400

    added textbook_create_table function

--- TextbookAnnotater.php
+++ TextbookAnnotater.php
@@ -27,6 +27,24 @@ function textbook_annotater_setup_menu(){
 }
 
 
+// Create DB table for textbooks
+register_activation_hook( __FILE__, 'Textbook_create_table' );
+function Textbook_create_table(){
+   
+   global $wpdb;
+   $table_name = $wpdb->prefix . 'textbooks';
+   $charset_collate = $wpdb->get_charset_collate();
 
+   $sql = "CREATE TABLE $table_name (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+      name tinytext NOT NULL,
+      author tinytext NOT NULL,
+      PRIMARY KEY  (id)
+   ) $charset_collate;";
+
+   require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+   dbDelta( $sql );
+}
 
 ?>
