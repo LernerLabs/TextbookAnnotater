a17a027db288bf68fb360f584df7215ecc5fd710
Michael G. Lerner <mglerner@pm.me>
2021-06-10 15:04:35 -0700

    indented properly because i am a professional

--- slide_generator.php
+++ slide_generator.php
@@ -18,71 +18,71 @@ use PhpOffice\PhpPresentation\Style\Alignment;
 # another takes a textbook ID.
 
 function generate_single_response_slide($currentSlide, $response){
-	 $image_filename = plugin_dir_path( __FILE__ ) . "./vendor/phpoffice/phppresentation/samples/resources/phppowerpoint_logo.gif";
-	 $image_filename = plugin_dir_path( __FILE__ ) . "Noether.jpg";
-	 $shape = $currentSlide->createDrawingShape();
-	 # We'll assume about 96 pixels per inch.
-	 $shape->setName('PHPPresentation logo')
-	       ->setDescription('PHPPresentation logo')
-	       ->setPath($image_filename)
-	       ->setHeight(5*96)
-	       ->setOffsetX(0.5*96)
-	       ->setOffsetY(0.5*96);
+    $image_filename = plugin_dir_path( __FILE__ ) . "./vendor/phpoffice/phppresentation/samples/resources/phppowerpoint_logo.gif";
+    $image_filename = plugin_dir_path( __FILE__ ) . "Noether.jpg";
+    $shape = $currentSlide->createDrawingShape();
+    # We'll assume about 96 pixels per inch.
+    $shape->setName('PHPPresentation logo')
+          ->setDescription('PHPPresentation logo')
+          ->setPath($image_filename)
+          ->setHeight(5*96)
+          ->setOffsetX(0.5*96)
+          ->setOffsetY(0.5*96);
 	# Give it a drop shadow
 	$shape->getShadow()->setVisible(true)
-              ->setDirection(45)
-              ->setDistance(10);
-        # And the text
+          ->setDirection(45)
+          ->setDistance(10);
+    # And the text
 	$shape = $currentSlide->createRichTextShape()
-			      ->setHeight(300)
-			      ->setWidth(600)
-			      ->setOffsetX(96*3)
-			      ->setOffsetY(180);
+                          ->setHeight(300)
+                          ->setWidth(600)
+                          ->setOffsetX(96*3)
+                          ->setOffsetY(180);
 	$shape->getActiveParagraph()->getAlignment()->setHorizontal( Alignment::HORIZONTAL_CENTER );
 	$textRun = $shape->createTextRun($response->student_name); # should be scientist name
 	$textRun->getFont()->setBold(true)
-		->setSize(60)
-		->setColor( new Color('FFE06B20'));
-
+            ->setSize(60)
+            ->setColor( new Color('FFE06B20'));
+    
 	# And the instructor notes
 	$oNote = $currentSlide->getNote();
 	$oProperties = $oPHPPresentation->getPresentationProperties();
 	$oProperties->setCommentVisible(true);
-
+    
 	$oRichText = $oNote->createRichTextShape()
 	                   ->setHeight(60)
-		           ->setWidth(60)
-		           ->setOffsetX(170)
-		           ->setOffsetY(180);
+                       ->setWidth(60)
+                       ->setOffsetX(170)
+                       ->setOffsetY(180);
 	$oRichText->createTextRun('Description of Scientist');
 	$oRichText->createParagraph()->createTextRun('Flork bork');
 	$oRichText->createParagraph()->createTextRun($response->description);
 }
 
 function generate_single_slide_presentation($response){
-        # Now make the PPTX files
+    # Now make the PPTX files
 	$oPHPPresentation = new PhpPresentation();
 	$currentSlide = $oPHPPresentation->getActiveSlide();
 	generate_single_response_slide($currentSlide, $response);
 	return $oPHPPresentation;
-#	# output would look like this
-#	       	 $outfname = preg_replace("/[^A-Za-z0-9]/", '', $response->student_name); # should be scientist name
-#        	 $outfname = $outfname . ".pptx";
-#
-#		 
-#        
-#        	 $oWriterPPTX = IOFactory::createWriter($oPHPPresentation, 'PowerPoint2007');
-#        	 $oWriterPPTX->save(__DIR__ . "/" . $outfname );
+    #	# output would look like this
+    #	       	 $outfname = preg_replace("/[^A-Za-z0-9]/", '', $response->student_name); # should be scientist name
+    #        	 $outfname = $outfname . ".pptx";
+    #
+    #		 
+    #        
+    #        	 $oWriterPPTX = IOFactory::createWriter($oPHPPresentation, 'PowerPoint2007');
+    #        	 $oWriterPPTX->save(__DIR__ . "/" . $outfname );
 }
 
 function generate_textbook_slide_deck($textbook_id){
-	 $all_student_responses = get_all_student_responses();
-	 $oPHPPresentation = new PhpPresentation();
-	 foreach($all_student_responses as $response){
-	         $currentSlide = $oPHPPresentation->createSlide();
-		 generate_single_response_slide($currentSlide, $response);
-	 }
-	 # Now output something
+    $all_student_responses = get_all_student_responses();
+    $oPHPPresentation = new PhpPresentation();
+    foreach($all_student_responses as $response){
+        $currentSlide = $oPHPPresentation->createSlide();
+        generate_single_response_slide($currentSlide, $response);
+    }
+    # Now output something
 }
 
 ?>
