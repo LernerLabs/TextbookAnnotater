5e43cbc6e85ced9fbd34520b80c810fc873a439f
AF3 <af@af3.tech>
2021-06-16 14:11:29 -0400

    deleted the code for textbook custom table which is not needed anymore

--- includes/textbook_controller.php
+++ includes/textbook_controller.php
@@ -67,55 +67,6 @@ function textbook_custom_post_template($single) {
 
 }
 
-// add student response form page for textbook
-function add_student_response_page($textbook_id, $texbook_name){
-	
-	$post_details = array(
-		'post_title'    => "Textbook " . $texbook_name . " Form",
-		'post_content'  => '[student_response_form textbook_id=' . $textbook_id . ']',
-		'post_status'   => 'publish',
-		'post_author'   => 1,
-		'post_type' => 'page'
-	);
-	$page_id = wp_insert_post( $post_details );
-
-	global $wpdb; 
-	$postsTable = $wpdb->prefix.'posts'; 
-	$page = $wpdb->get_results ( "SELECT * FROM $postsTable WHERE ID = $page_id");
-	$textbookTable = $wpdb->prefix.'textbooks'; 
-	$texbook = $wpdb->get_results ( "SELECT * FROM $textbookTable WHERE id = $textbook_id");
-	$wpdb->update($textbookTable, array("form_page_url" => $page[0]->guid), array('id' => $textbook_id));
-
-}
-
-// add approved student responses page for textbook
-function add_approved_student_responses_page($textbook_id, $texbook_name){
-	
-	$post_details = array(
-		'post_title'    => "Textbook " . $texbook_name . " Responses",
-		'post_content'  => '[approved_student_responses textbook_id=' . $textbook_id . ']',
-		'post_status'   => 'publish',
-		'post_author'   => 1,
-		'post_type' => 'page'
-	);
-	$page_id = wp_insert_post( $post_details );
-
-	global $wpdb; 
-	$postsTable = $wpdb->prefix.'posts'; 
-	$page = $wpdb->get_results ( "SELECT * FROM $postsTable WHERE ID = $page_id");
-	$textbookTable = $wpdb->prefix.'textbooks'; 
-	$texbook = $wpdb->get_results ( "SELECT * FROM $textbookTable WHERE id = $textbook_id");
-	$wpdb->update($textbookTable, array("responses_page_url" => $page[0]->guid), array('id' => $textbook_id));
-
-}
-
-// get textbook by id
-function get_textbook_by_id($id){
-	global $wpdb;    
-	$textbookTable = $wpdb->prefix.'textbooks';
-	$result = $wpdb->get_results ( "SELECT * FROM $textbookTable WHERE id = $id");
-	return $result;
-}
 
 
 // get all textbooks from database
@@ -151,12 +102,4 @@ function add_new_textbook($name, $author){
         '_textbook_annotator_author_meta_key',
         $author
     );
-}
-
-
-// delete textbook from database
-function delete_textbook($id){
-	global $wpdb;
-	$table_name = $wpdb->prefix . 'textbooks';
-	$wpdb->delete( $table_name, array( 'id' => $id ) );
 }
