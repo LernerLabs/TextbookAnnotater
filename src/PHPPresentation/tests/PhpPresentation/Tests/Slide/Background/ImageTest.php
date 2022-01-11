6b85c772806ef342384d3301d6fa18b9d8737e95
Michael G. Lerner <mglerner@pm.me>
2022-01-11 13:35:45 -0500

    Added PHPPresentation and Common. Recall that this is why we changed our license, so that we could just include it raw.

---
+++ src/PHPPresentation/tests/PhpPresentation/Tests/Slide/Background/ImageTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace PhpOffice\PhpPresentation\Tests\Slide\Background;
+
+use PhpOffice\PhpPresentation\Slide\Background\Image;
+use PHPUnit\Framework\TestCase;
+
+class ImageTest extends TestCase
+{
+    public function testColor()
+    {
+        $object = new Image();
+
+        $imagePath = PHPPRESENTATION_TESTS_BASE_DIR.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'PhpPresentationLogo.png';
+        $numSlide = mt_rand(1, 100);
+
+        $this->assertNull($object->getPath());
+        $this->assertEmpty($object->getFilename());
+        $this->assertEmpty($object->getExtension());
+        $this->assertEquals('background_' . $numSlide . '.', $object->getIndexedFilename($numSlide));
+
+        $this->assertInstanceOf('PhpOffice\\PhpPresentation\\Slide\\Background\\Image', $object->setPath($imagePath));
+        $this->assertEquals($imagePath, $object->getPath());
+        $this->assertEquals('PhpPresentationLogo.png', $object->getFilename());
+        $this->assertEquals('png', $object->getExtension());
+        $this->assertEquals('background_' . $numSlide . '.png', $object->getIndexedFilename($numSlide));
+
+        $this->assertInstanceOf('PhpOffice\\PhpPresentation\\Slide\\Background\\Image', $object->setPath(null, false));
+        $this->assertNull($object->getPath());
+        $this->assertEmpty($object->getFilename());
+        $this->assertEmpty($object->getExtension());
+        $this->assertEquals('background_' . $numSlide . '.', $object->getIndexedFilename($numSlide));
+    }
+
+    public function testPathException()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('File not found :');
+
+        $object = new Image();
+        $object->setPath('pathDoesntExist', true);
+    }
+}
